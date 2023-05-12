ce2bcec2993ec944f4230eb763ca7176a3bf25b9
Andrei Romanouski <@aromanouski1>
2023-05-12 08:41:26 +0400

    create product controller

--- product-service/src/main/java/com/appsdev/estore/product/controller/ProductController.java
+++ product-service/src/main/java/com/appsdev/estore/product/controller/ProductController.java
@@ -2,7 +2,10 @@ package com.appsdev.estore.product.controller;
 
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,30 @@ public class ProductController {
     public List<String> getProducts(){
         log.info("getProducts");
 
-
         return List.of("hi", "hi2");
+    }
+
+    @PostMapping
+    public String createProduct(){
+        log.info("createProduct");
+
+
+        return "Create product";
+    }
+
+    @PutMapping
+    public String updateProduct(){
+        log.info("updateProduct");
+
+
+        return "Update product";
+    }
+
+    @DeleteMapping
+    public String deleteProduct(){
+        log.info("deleteProduct");
+
+
+        return "Delete product";
     }
 }
