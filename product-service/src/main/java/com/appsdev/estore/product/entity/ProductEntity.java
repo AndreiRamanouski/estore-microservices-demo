6f9dfa7e4de90fecce1f87a09755fac14afc85b2
Andrei Romanouski <@aromanouski1>
2023-05-14 19:49:54 +0400

    implement CQRS - Add database - Persist products

---
+++ product-service/src/main/java/com/appsdev/estore/product/entity/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.appsdev.estore.product.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "products")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity implements Serializable {
+
+    @Id
+    @Column(unique = true)
+    private String productId;
+    @Column(unique = true)
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ product-service/src/main/java/com/appsdev/estore/product/projection/ProductProjection.java
@@ -0,0 +1,29 @@
+package com.appsdev.estore.product.projection;
+
+import com.appsdev.estore.product.entity.ProductEntity;
+import com.appsdev.estore.product.events.ProductCreateEvent;
+import com.appsdev.estore.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductProjection {
+
+    private final ProductRepository productRepository;
+
+    @EventHandler
+    public void on(ProductCreateEvent event){
+        log.info("Persist product with id {}", event.getProductId());
+        ProductEntity saved = productRepository.save(ProductEntity.builder()
+                .productId(event.getProductId())
+                .title(event.getTitle())
+                .price(event.getPrice())
+                .quantity(event.getQuantity())
+                .build());
+        log.info("Product has been saved id: {}", saved.getProductId());
+    }
+}
