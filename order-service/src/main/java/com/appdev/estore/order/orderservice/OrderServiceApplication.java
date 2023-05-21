37a301a0343b7284dbfe64f6051e4ecd1c281a0f
Andrei Romanouski <@aromanouski1>
2023-05-21 08:48:38 +0400

    Deadlines - implement deadline for payment

--- order-service/src/main/java/com/appdev/estore/order/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/appdev/estore/order/orderservice/OrderServiceApplication.java
@@ -1,11 +1,17 @@
 package com.appdev.estore.order.orderservice;
 
 import com.appdev.estore.order.orderservice.error.OrdersServiceEventsErrorHandler;
+import org.axonframework.config.Configuration;
+import org.axonframework.config.ConfigurationScopeAwareProvider;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -21,5 +27,13 @@ public class OrderServiceApplication {
     public void configure(EventProcessingConfigurer configurer) {
         configurer.registerListenerInvocationErrorHandler("order-group",
                 conf -> new OrdersServiceEventsErrorHandler());
+    }
+
+    @Bean
+    public DeadlineManager deadlineManager(Configuration configuration, SpringTransactionManager transactionManager){
+        return SimpleDeadlineManager.builder()
+                .scopeAwareProvider(new ConfigurationScopeAwareProvider(configuration))
+                .transactionManager(transactionManager)
+                .build();
     }
 }
--- order-service/src/main/java/com/appdev/estore/order/orderservice/saga/OrderSaga.java
+++ order-service/src/main/java/com/appdev/estore/order/orderservice/saga/OrderSaga.java
@@ -13,9 +13,10 @@ import com.appdev.estore.order.orderservice.command.commands.RejectOrderCommand;
 import com.appdev.estore.order.orderservice.command.event.OrderApprovedEvent;
 import com.appdev.estore.order.orderservice.command.event.OrderCreateEvent;
 import com.appdev.estore.order.orderservice.command.event.OrderRejectEvent;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,8 @@ import org.axonframework.commandhandling.CommandCallback;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.annotation.DeadlineHandler;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.EndSaga;
 import org.axonframework.modelling.saga.SagaEventHandler;
@@ -42,17 +45,21 @@ public class OrderSaga {
     @Autowired
     private transient QueryGateway queryGateway;
 
+    @Autowired
+    private transient DeadlineManager deadlineManager;
+
+    private static final String PAYMENT_PROCESSING_DEADLINE = "payment-processing-deadline";
+
+    private String scheduleId = null;
+
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderCreateEvent orderCreateEvent) {
         log.info("handle OrderCreateEvent");
 
-        ReserveProductCommand command = ReserveProductCommand.builder()
-                .orderId(orderCreateEvent.getOrderId())
-                .productId(orderCreateEvent.getProductId())
-                .quantity(orderCreateEvent.getQuantity())
-                .userId(orderCreateEvent.getUserId())
-                .build();
+        ReserveProductCommand command = ReserveProductCommand.builder().orderId(orderCreateEvent.getOrderId())
+                .productId(orderCreateEvent.getProductId()).quantity(orderCreateEvent.getQuantity())
+                .userId(orderCreateEvent.getUserId()).build();
         log.info("Order event handled for orderId {} and productId {}", command.getOrderId(), command.getProductId());
         commandGateway.send(command, new CommandCallback<ReserveProductCommand, Object>() {
             @Override
@@ -90,16 +97,23 @@ public class OrderSaga {
         log.info("Found user with id {}, and card payment name {}", user.getUserId(),
                 user.getPaymentDetails().getName());
 
-        ProcessPaymentCommand command = ProcessPaymentCommand.builder()
-                .orderId(productReservedEvent.getOrderId())
-                .paymentId(UUID.randomUUID().toString())
-                .paymentDetails(user.getPaymentDetails())
-                .build();
+        ProcessPaymentCommand command = ProcessPaymentCommand.builder().orderId(productReservedEvent.getOrderId())
+                .paymentId(UUID.randomUUID().toString()).paymentDetails(user.getPaymentDetails()).build();
+
+        //the id can be used to cancel a particular deadline by the returned schedule id
+        scheduleId = deadlineManager.schedule(Duration.of(10, ChronoUnit.SECONDS), PAYMENT_PROCESSING_DEADLINE,
+                productReservedEvent);
+        // true to test the deadline
+        //        if (true) {
+        //            return;
+        //        }
 
         String paymentResult = null;
         try {
             log.info("Sent payment for verification");
-            paymentResult = commandGateway.sendAndWait(command, 10, TimeUnit.SECONDS);
+            //            paymentResult = commandGateway.sendAndWait(command, 10, TimeUnit.SECONDS);
+            //Use deadline manager
+            paymentResult = commandGateway.sendAndWait(command);
             log.info("Payment result {}", paymentResult);
         } catch (Exception exception) {
             log.error("Exception {}", exception.getLocalizedMessage());
@@ -117,19 +131,19 @@ public class OrderSaga {
 
     private void cancelProductReservation(ProductReservedEvent event, String reason) {
         log.info("cancelProductReservation for product id {}", event.getProductId());
-        commandGateway.send(CancelProductReservationCommand.builder()
-                .orderId(event.getOrderId())
-                .productId(event.getProductId())
-                .quantity(event.getQuantity())
-                .userId(event.getUserId())
-                .reason(reason)
-                .build());
+        cancelDeadline(PAYMENT_PROCESSING_DEADLINE);
+        commandGateway.send(
+                CancelProductReservationCommand.builder().orderId(event.getOrderId()).productId(event.getProductId())
+                        .quantity(event.getQuantity()).userId(event.getUserId()).reason(reason).build());
     }
 
 
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(PaymentProcessedEvent event) {
         log.info("handle PaymentProcessedEvent");
+        //cancel deadline
+        cancelDeadline(PAYMENT_PROCESSING_DEADLINE);
+
         ApproveOrderCommand command = ApproveOrderCommand.builder().orderId(event.getOrderId()).build();
         commandGateway.send(command);
     }
@@ -139,8 +153,7 @@ public class OrderSaga {
     public void handle(OrderApprovedEvent event) {
         log.info("handle OrderApprovedEvent");
         log.info("The order SAGA has been completed!!! The order is approved. Order id {} and status {}",
-                event.getOrderId(),
-                event.getOrderStatus());
+                event.getOrderId(), event.getOrderStatus());
         //add response entity
         //        SagaLifecycle.end(); the same as @EndSaga
     }
@@ -158,6 +171,22 @@ public class OrderSaga {
         log.info("The order saga has been completed!!! The order has been canceled. Order id {}, reason {}",
                 event.getOrderId(), event.getReason());
 
+    }
+
+    @DeadlineHandler(deadlineName = PAYMENT_PROCESSING_DEADLINE)
+    public void handleDeadline(ProductReservedEvent productReservedEvent) {
+        log.info("Payment processing deadline was called for order id {}, starting compensating transaction",
+                productReservedEvent.getOrderId());
+        cancelProductReservation(productReservedEvent, "Payment timeout");
+    }
+
+    private void cancelDeadline(String name) {
+
+        if (Objects.nonNull(scheduleId)) {
+            deadlineManager.cancelSchedule(name, scheduleId);
+            scheduleId = null;
+        }
+        //        deadlineManager.cancelAll(name);
     }
 
 }
